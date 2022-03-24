223099654221dcd95ce3592603b98f5e2663d1a0
armandios19 <fooseaov@gmail.com>
2022-03-24 10:19:45 -0600

    security_domain_df function

--- scripts/funciones_data_frame.R
+++ scripts/funciones_data_frame.R
@@ -32,3 +32,33 @@ security_state_df <- function(x = seguridad_alimentaria, state){
 }
 
 security_state_df(state = "Morelos")
+
+
+security_domain_df <- function(x = seguridad_alimentaria){
+  rur <- x %>%
+    select(alimentaria, dominio) %>%
+    group_by(alimentaria, dominio) %>%
+    count() %>%
+    filter(dominio == "rural") %>%
+    summarise(total = sum(freq))
+
+  rur_total <- rur$total
+
+  urb <- x %>%
+    select(alimentaria, dominio) %>%
+    group_by(alimentaria, dominio) %>%
+    count() %>%
+    filter(dominio == "urbano") %>%
+    summarise(total = sum(freq))
+
+  urb_total <- urb$total
+
+  x %>%
+    select(alimentaria, dominio) %>%
+    group_by(alimentaria, dominio) %>%
+    count() %>%
+    mutate(porcentaje = case_when(dominio == "urbano" ~ freq/urb_total,
+                                  dominio == "rural" ~ freq/rur_total))
+}
+
+security_domain_df()
