7dfd24f3b7e637d76d85dc6e263577be036fcd3e
armandios19 <fooseaov@gmail.com>
2022-03-23 23:06:30 -0600

    security_domain_state_plot function

--- scripts/funciones.R
+++ scripts/funciones.R
@@ -54,7 +54,7 @@ security_state_plot <- function(x = seguridad_alimentaria, state){
 
 security_state_plot(state = "Morelos")
 
-# funcion para graficar seguridad alimentaria por dominio
+### funcion para graficar seguridad alimentaria por dominio
 
 security_domain_plot <- function(x = seguridad_alimentaria){
   rur <- x %>%
@@ -94,3 +94,52 @@ security_domain_plot <- function(x = seguridad_alimentaria){
 }
 
 security_domain_plot()
+
+
+### funcion para graficar seguridad alimentaria por dominio y estado
+
+security_domain_state_plot <- function(x = seguridad_alimentaria, state){
+  rur <- x %>%
+    select(alimentaria, dominio, entidad) %>%
+    filter(entidad == state) %>%
+    group_by(alimentaria, dominio) %>%
+    count() %>%
+    filter(dominio == "rural") %>%
+    summarise(total = sum(freq))
+
+  rur_total <- rur$total
+
+  urb <- x %>%
+    select(alimentaria, dominio, entidad) %>%
+    filter(entidad == state) %>%
+    group_by(alimentaria, dominio) %>%
+    count() %>%
+    filter(dominio == "urbano") %>%
+    summarise(total = sum(freq))
+
+  urb_total <- urb$total
+
+  x %>%
+    select(alimentaria, dominio, entidad) %>%
+    filter(entidad == state) %>%
+    group_by(alimentaria, dominio) %>%
+    count() %>%
+    mutate(porcentaje = case_when(dominio == "urbano" ~ freq/urb_total,
+                                  dominio == "rural" ~ freq/rur_total)) %>%
+    ggplot(aes(x = alimentaria, y = porcentaje, fill = alimentaria))+
+    geom_col()+
+    scale_x_discrete(limits=c("seguridad_alimentaria","inseguridad_leve",
+                              "inseguridad_moderada", "inseguridad_severa"))+
+    ylab("")+
+    xlab("")+
+    ylim(0,1)+
+    theme_grey()+
+    theme(legend.position="none")+
+    facet_wrap(.~dominio)
+}
+
+security_domain_state_plot(state = "Aguascalientes")
+
+
+
+
