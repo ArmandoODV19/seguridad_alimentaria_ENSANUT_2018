df6cef14017c61a8f3e11d978bf953c5d282d4ad
armandios19 <fooseaov@gmail.com>
2022-03-24 10:14:52 -0600

    security_state_df function

--- scripts/funciones_data_frame.R
+++ scripts/funciones_data_frame.R
@@ -1,2 +1,21 @@
 ### funciones para obtener data frames
 
+security_state_df <- function(x = seguridad_alimentaria, state){
+  sta <- x %>%
+    select(alimentaria, entidad) %>%
+    filter(entidad == state) %>%
+    group_by(alimentaria, entidad) %>%
+    count() %>%
+    summarise(total = sum(freq))
+
+  sta_total <- sta$total
+
+  x %>%
+    filter(entidad == state) %>%
+    select(alimentaria) %>%
+    group_by(alimentaria) %>%
+    count() %>%
+    mutate(porcentaje = freq/sta_total)
+}
+
+security_state_df(state = "Morelos")
