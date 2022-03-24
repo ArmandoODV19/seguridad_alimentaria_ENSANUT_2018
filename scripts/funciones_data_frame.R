e53516e0cbb8f10494be5f6b1ce72e6f2083b466
armandios19 <fooseaov@gmail.com>
2022-03-24 10:22:34 -0600

    security_domain_state_df

--- scripts/funciones_data_frame.R
+++ scripts/funciones_data_frame.R
@@ -62,3 +62,36 @@ security_domain_df <- function(x = seguridad_alimentaria){
 }
 
 security_domain_df()
+
+
+security_domain_state_df <- function(x = seguridad_alimentaria, state){
+  rur <- x %>%
+    select(alimentaria, dominio, entidad) %>%
+    filter(entidad == state) %>%
+    group_by(alimentaria, dominio) %>%
+    count() %>%
+    filter(dominio == "rural") %>%
+    summarise(total = sum(freq))
+
+  rur_total <- rur$total
+
+  urb <- x %>%
+    select(alimentaria, dominio, entidad) %>%
+    filter(entidad == state) %>%
+    group_by(alimentaria, dominio) %>%
+    count() %>%
+    filter(dominio == "urbano") %>%
+    summarise(total = sum(freq))
+
+  urb_total <- urb$total
+
+  x %>%
+    select(alimentaria, dominio, entidad) %>%
+    filter(entidad == state) %>%
+    group_by(alimentaria, dominio) %>%
+    count() %>%
+    mutate(porcentaje = case_when(dominio == "urbano" ~ freq/urb_total,
+                                  dominio == "rural" ~ freq/rur_total))
+}
+
+security_domain_state_df(state = "Aguascalientes")
