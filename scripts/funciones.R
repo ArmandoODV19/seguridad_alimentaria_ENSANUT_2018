03dcd73d014cd57a8d947d5ef88115cf9c392dd5
armandios19 <fooseaov@gmail.com>
2022-03-23 21:50:24 -0600

    security_plot function

--- scripts/funciones.R
+++ scripts/funciones.R
@@ -1,1 +1,21 @@
 #### generando funciones
+
+security_plot <- function(x = seguridad_alimentaria){
+  x %>%
+    select(alimentaria) %>%
+    group_by(alimentaria) %>%
+    count() %>%
+    mutate(porcentaje = freq/44151) %>%
+    ggplot(aes(x = alimentaria, y = porcentaje, fill = alimentaria))+
+    scale_x_discrete(limits=c("seguridad_alimentaria","inseguridad_leve",
+                              "inseguridad_moderada", "inseguridad_severa"))+
+    geom_col()+
+    ylab("")+
+    xlab("")+
+    ylim(0,1)+
+    theme_grey()+
+    theme(legend.position="none")
+}
+
+security_plot()
+
