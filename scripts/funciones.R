01cc5953d16ef001b473cde7477e6f2ec09edc22
armandios19 <fooseaov@gmail.com>
2022-03-23 22:41:03 -0600

    security_state_plot function

--- scripts/funciones.R
+++ scripts/funciones.R
@@ -1,5 +1,8 @@
 #### generando funciones
 
+
+# funcion para obtner seguridad alimentaria a nivel nacional
+
 security_plot <- function(x = seguridad_alimentaria){
   x %>%
     select(alimentaria) %>%
@@ -18,4 +21,36 @@ security_plot <- function(x = seguridad_alimentaria){
 }
 
 security_plot()
+
+
+# funcion para obtener seguridad alimentaria por estado
+
+security_state_plot <- function(x = seguridad_alimentaria, state){
+  sta <- x %>%
+    select(alimentaria, entidad) %>%
+    filter(entidad == state) %>%
+    group_by(alimentaria, entidad) %>%
+    count() %>%
+    summarise(total = sum(freq))
+
+  sta_total <- sta$total
+
+  x %>%
+    filter(entidad == state) %>%
+    select(alimentaria) %>%
+    group_by(alimentaria) %>%
+    count() %>%
+    mutate(porcentaje = freq/sta_total) %>%
+    ggplot(aes(x = alimentaria, y = porcentaje, fill = alimentaria))+
+    geom_col()+
+    scale_x_discrete(limits=c("seguridad_alimentaria","inseguridad_leve",
+                              "inseguridad_moderada", "inseguridad_severa"))+
+    ylab("")+
+    xlab("")+
+    ylim(0,1)+
+    theme_grey()+
+    theme(legend.position="none")
+}
+
+security_state_plot(state = "Morelos")
 
