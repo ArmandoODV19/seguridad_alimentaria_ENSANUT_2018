4c2299e8571138b55a19da31e663ff25d6f3bd02
armandios19 <fooseaov@gmail.com>
2022-03-25 10:48:26 -0600

    security_zone_plot

--- scripts/funciones.R
+++ scripts/funciones.R
@@ -141,5 +141,34 @@ security_domain_state_plot <- function(x = seguridad_alimentaria, state){
 security_domain_state_plot(state = "Morelos")
 
 
+### funcion para graficar seguridad alimentaria por region
 
 
+security_zone_plot <- function(x = seguridad_alimentaria, zone){
+  reg <- x %>%
+    select(alimentaria, region) %>%
+    filter(region == zone) %>%
+    group_by(alimentaria, region) %>%
+    count() %>%
+    summarise(total = sum(freq))
+
+  reg_total <- reg$total
+
+  x %>%
+    filter(region == zone) %>%
+    select(alimentaria) %>%
+    group_by(alimentaria) %>%
+    count() %>%
+    mutate(porcentaje = freq/reg_total) %>%
+    ggplot(aes(x = alimentaria, y = porcentaje, fill = alimentaria))+
+    geom_col()+
+    scale_x_discrete(limits=c("seguridad_alimentaria","inseguridad_leve",
+                              "inseguridad_moderada", "inseguridad_severa"))+
+    ylab("")+
+    xlab("")+
+    ylim(0,1)+
+    theme_grey()+
+    theme(legend.position="none")
+}
+
+security_zone_plot(zone = "centro")
