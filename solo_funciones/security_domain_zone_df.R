cf595789bb06910d0c958bf2067bc88358c8c0d0
armandios19 <fooseaov@gmail.com>
2022-03-28 15:35:04 -0600

    security_domain_zone_df function

---
+++ solo_funciones/security_domain_zone_df.R
@@ -0,0 +1,29 @@
+security_domain_zone_df <- function(x = seguridad_alimentaria, zone){
+  rur <- x %>%
+    select(alimentaria, dominio, region) %>%
+    filter(region == zone) %>%
+    group_by(alimentaria, dominio) %>%
+    count() %>%
+    filter(dominio == "rural") %>%
+    summarise(total = sum(freq))
+
+  rur_total <- rur$total
+
+  urb <- x %>%
+    select(alimentaria, dominio, region) %>%
+    filter(region == zone) %>%
+    group_by(alimentaria, dominio) %>%
+    count() %>%
+    filter(dominio == "urbano") %>%
+    summarise(total = sum(freq))
+
+  urb_total <- urb$total
+
+  x %>%
+    select(alimentaria, dominio, region) %>%
+    filter(region == zone) %>%
+    group_by(alimentaria, dominio) %>%
+    count() %>%
+    mutate(porcentaje = case_when(dominio == "urbano" ~ freq/urb_total,
+                                  dominio == "rural" ~ freq/rur_total))
+}
