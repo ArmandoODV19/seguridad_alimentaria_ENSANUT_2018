b55ca6e57154cf9c2f71bef3bc5294c70c8cdac6
armandios19 <fooseaov@gmail.com>
2022-03-28 15:17:25 -0600

    security_plot function

---
+++ solo_funciones/security_plot.R
@@ -0,0 +1,16 @@
+security_plot <- function(x = seguridad_alimentaria){
+  x %>%
+    select(alimentaria) %>%
+    group_by(alimentaria) %>%
+    count() %>%
+    mutate(porcentaje = freq/44151) %>%
+    ggplot(aes(x = alimentaria, y = porcentaje, fill = alimentaria))+
+    scale_x_discrete(limits=c("seguridad_alimentaria","inseguridad_leve",
+                              "inseguridad_moderada", "inseguridad_severa"))+
+    geom_col()+
+    ylab("")+
+    xlab("")+
+    ylim(0,1)+
+    theme_grey()+
+    theme(legend.position="none")
+}
